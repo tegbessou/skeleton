0a56ab99e2a7cfe39c0bd6970e2320cebd2bc652
tegbessou <hugues.gobet@gmail.com>
2020-10-28 23:15:39 +0100

    Add webpack encore, first scss file and js file, install eslint and makefile command to run it

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
